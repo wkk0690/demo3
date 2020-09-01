7a1bc5c89eb858ab659c26b0b45559b102081ec7
wkk <wkk0690@126.com>
2020-09-01 16:58:11 +0800

    111

---
+++ src/main/java/com/example/demo3/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wkk
+ * @create 2020/08/25
+ * @desc
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/test1")
+    public String test1(){
+        List<String> list = new ArrayList();
+        return "123";
+    }
+}
