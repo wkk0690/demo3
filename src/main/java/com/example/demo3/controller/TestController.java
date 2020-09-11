e5474093d3d1847398f4de22cd39b538c1368482
wkk <wkk0690@126.com>
2020-09-11 15:28:05 +0800

    11

--- src/main/java/com/example/demo3/controller/TestController.java
+++ src/main/java/com/example/demo3/controller/TestController.java
@@ -18,6 +18,6 @@ public class TestController {
     @RequestMapping("/test1")
     public String test1(){
         List<String> list = new ArrayList();
-        return "123世界你好1111111111111";
+        return "123";
     }
 }
