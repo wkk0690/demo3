6c8f1855da2d735c6a894316b5d9aa10d61d9577
0X574B4B <wkk0690@126.com>
2020-10-09 13:07:00 +0800

    Update TestController.java

--- src/main/java/com/example/demo3/controller/TestController.java
+++ src/main/java/com/example/demo3/controller/TestController.java
@@ -18,6 +18,6 @@ public class TestController {
     @RequestMapping("/test1")
     public String test1(){
         List<String> list = new ArrayList();
-        return "第3次部署";
+        return "第4次部署";
     }
 }
